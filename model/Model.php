c0ed8a389b88dcbe05d1d04a20f55e8a41d15db1
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2018-05-31 22:31:21 -0300

    Conexion Postgres, y createTable

--- model/Model.php
+++ model/Model.php
@@ -8,17 +8,16 @@ define('DB_FILE','SQL/db_departamentos.sql');
 class Model
 {
   protected $db;
-  protected $host = "http://dbases.exa.unicen.edu.ar:8080";
+  protected $host = "dbases.exa.unicen.edu.ar";
 
   function __construct()
-  {
+  { 
     try {
-       // $this->db = new PDO('mysql:host='.DB_HOSTNAME . '; dbname='.DB_NAME. ';charset=utf8', DB_USER,DB_PASSWORD);
-        $this->db = new PDO("pgsql:host=$host; port=6432; dbname=cursada", 'unc_246336', 'ibanez011');
+        $this->db = new PDO("pgsql:host=localhost;port=5432; dbname=departamentos", 'postgres', 'system011');
+        $this->db->exec('SET search_path TO public');
     }
     catch (PDOException $e){
       echo "ERROR: Trying to connect to the Database";
-      echo $db;
       die();
     }
   }
